0dd09f277a700182495c4fc687b4ed1d16c2ccab
keeley <keeley.jun@qq.com>
2019-11-27 10:52:28 +0800

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 # build源代码并且提交
 yarn pro && \
 cd dist && \
+ls -l && \
 rm -rf .git && \
 git init && \
 git add . && \
 git commit -m "auto commit" && \
 git remote add origin git@github.com:keeleys/keeleys.github.com.git && \
 git push -u origin master -f
+cd ../
